74fa4b7272d9f591ce1c0cfedc870052f3cd47be
TrungKenbi <trungkenbi@hotmail.com>
2021-08-08 02:53:02 +0700

    Fix

--- Arduino/DoorSystem/DoorSystem.ino
+++ Arduino/DoorSystem/DoorSystem.ino
@@ -25,47 +25,6 @@ void openDoor();
 void closeDoor();
 void alert();
 
-// Xử lý dữ liệu nhận về từ Serial bằng ngắt
-void serialEvent() {
-  while (Serial.available()) {
-    int inp = Serial.read();
-    if (inp == 10)
-      break;
-    if (inp == 48) {
-      // Lệnh đóng cửa
-      digitalWrite(LED_BUILTIN, LOW);
-      closeDoor();
-      alert();
-    } else if (inp == 49) {
-      // Lệnh mở cửa
-      digitalWrite(LED_BUILTIN, HIGH);
-      openDoor();
-      alert();
-    }
-  }
-}
-
-// Xử lý nút nhấn bằng ngắt
-void buttonHandle() {
-  long currentMilis = millis();
-  bool currentState = digitalRead(BUTTON_PIN);
-  if (currentState == HIGH) {
-    pressedTime = currentMilis;
-    digitalWrite(LED_PIN, HIGH);
-    tone(BUZZER_PIN, 1000);
-  } else if (currentState == LOW) {
-    releasedTime = currentMilis;
-    long pressDuration = releasedTime - pressedTime;
-    
-    // Nếu nút nhấn nhấn đủ lâu sẽ kích hoạt đóng cửa
-    if (pressDuration > LONG_PRESS_TIME)
-      closeDoor();
-      
-    digitalWrite(LED_PIN, LOW);
-    noTone(BUZZER_PIN);
-  }
-}
-
 void setup() {
   // Khởi tạo
   Serial.begin(115200);
@@ -113,6 +72,47 @@ void loop() {
   // Chờ servo đến khi servo quay xong
   while (!doorServo.update())
     delay(REFRESH_INTERVAL / 1000);
+}
+
+// Xử lý dữ liệu nhận về từ Serial bằng ngắt
+void serialEvent() {
+  while (Serial.available()) {
+    int inp = Serial.read();
+    if (inp == 10)
+      break;
+    if (inp == 48) {
+      // Lệnh đóng cửa
+      digitalWrite(LED_BUILTIN, LOW);
+      closeDoor();
+      alert();
+    } else if (inp == 49) {
+      // Lệnh mở cửa
+      digitalWrite(LED_BUILTIN, HIGH);
+      openDoor();
+      alert();
+    }
+  }
+}
+
+// Xử lý nút nhấn bằng ngắt
+void buttonHandle() {
+  long currentMilis = millis();
+  bool currentState = digitalRead(BUTTON_PIN);
+  if (currentState == HIGH) {
+    pressedTime = currentMilis;
+    digitalWrite(LED_PIN, HIGH);
+    tone(BUZZER_PIN, 1000);
+  } else if (currentState == LOW) {
+    releasedTime = currentMilis;
+    long pressDuration = releasedTime - pressedTime;
+    
+    // Nếu nút nhấn nhấn đủ lâu sẽ kích hoạt đóng cửa
+    if (pressDuration > LONG_PRESS_TIME)
+      closeDoor();
+      
+    digitalWrite(LED_PIN, LOW);
+    noTone(BUZZER_PIN);
+  }
 }
 
 // Mở cửa
